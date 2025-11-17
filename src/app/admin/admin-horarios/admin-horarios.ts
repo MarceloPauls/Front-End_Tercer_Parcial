07529b07eb1d73e15c3ea9116f9153a9fb2a1d30
Marcelo Pauls <paulsmarcelo@live.com>
2025-11-17 08:33:02 -0300

    mat-chip-list chnaged to mat-chip-set to resolve importing issues

--- src/app/admin/admin-horarios/admin-horarios.ts
+++ src/app/admin/admin-horarios/admin-horarios.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
 import { StorageService } from '../../core/services/storage.service';
 import { Restaurante } from '../../core/models/restaurante.model';
 import { Zona } from '../../core/models/zona.model';
@@ -12,7 +13,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-import { MatChipsModule } from '@angular/material/chips'; 
+
+
 
 @Component({
   selector: 'app-admin-horarios',
